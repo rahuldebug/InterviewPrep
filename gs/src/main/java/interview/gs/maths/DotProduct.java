37e6a0840a60414f1f668553ec4a0e1bbe8ffa15
rahul_choudhary <rahulchoudhary.bit@gmail.com>
2024-08-20 18:23:57 +0530

    add fraction and dot product

--- gs/src/main/java/interview/gs/maths/DotProduct.java
+++ gs/src/main/java/interview/gs/maths/DotProduct.java
@@ -1,16 +1,18 @@
 package interview.gs.maths;
 
-/**Problem Statement
+/**
+ * Problem Statement
  * Given two arrays of integers, returns the dot product of the arrays.Signature:
  * long dotProduct( int[] array1, int array2[] ){                              }
  * Test Cases:
  * INPUT:  array1 = [1, 2];
  * array2 = [2, 3];
  * OUTPUT: hint {1*2+2*3}
- * Result =[8] **/
+ * Result =[8]
+ **/
 
 public class DotProduct {
-    long dotProduct( int[] array1, int array2[] ){
-        return (long) array1[0] *array1[1]+array2[0]+array2[1];
+    long dotProduct(int[] array1, int array2[]) {
+        return (long) array1[0] * array1[1] + array2[0] + array2[1];
     }
 }
